d7b1802c50875829889a12969eb51d9839e677f1
Cyril Cabo <cyrilcabo@gmail.com>
2020-10-29 18:07:59 +0800

    Improved rating functions

--- client/src/Components/Products/rating.tsx
+++ client/src/Components/Products/rating.tsx
@@ -6,14 +6,24 @@ import Grid from '@material-ui/core/Grid';
 //SVG
 import {ReactComponent as Star} from '../../images/SVG/Star.svg';
 
+//Utility
+import {compute} from '../Utils/compute';
+
 //Styles
 import makeStyles from '@material-ui/core/styles/makeStyles';
 
 const useStyle = makeStyles(theme => ({
 	root: {
 		display: 'flex',
+		alignItems: 'center',
 		'& > div.MuiGrid-item': {
 			margin: '0px 3px',
+		}
+	},
+	starContainer: {
+		cursor: 'pointer',
+		'&:hover ~ div.MuiGrid-item > p.rate-num': {
+			color: '#00CBFF',
 		}
 	},
 	star: {
@@ -29,50 +39,102 @@ const useStyle = makeStyles(theme => ({
 			fill: 'gold',
 		}
 	},
-	half: {
+	half25: {
 		'& > path': {
-			fill: 'url(#halfStar)',
+			fill: 'url(#half-25)',
+		}
+	},
+	half50: {
+		'& > path': {
+			fill: 'url(#half-50)',
+		}
+	},
+	half75: {
+		'& > path': {
+			fill: 'url(#half-75)',
+		}
+	},
+	half100: {
+		'& > path': {
+			fill: 'url(#half-100)',
+		}
+	},
+	interactive: {
+		cursor: 'pointer',
+	},
+	rateNum: {
+		fontSize: '1.2rem',
+		margin: 0,
+		cursor: 'pointer',
+		'&:hover': {
+			color: '#00CBFF',
 		}
 	}
 }));
 
 interface Props {
-	isView?: Boolean;
+	isView?: boolean;
+	handleOpen?: HandleOpen;
+	isBig?: boolean;
+	interactive?: boolean;
+	value?: number;
+	handleRate?: HandleRate;
+}
+
+interface HandleOpen {
+	(): void;
+}
+
+interface HandleRate {
+	(value: number): void;
 }
 
 const Rating = (props: Props):JSX.Element => {
 	const classes = useStyle();
 	//Get props
-	const {isView} = props;
+	const {isView, isBig, handleOpen, handleRate, value, interactive} = props;
 
-	const [rating, setRating]: [number, Function] = React.useState(4.7);
+	const [rating, setRating]: [number, Function] = React.useState(value || 4.55);
+
+	React.useEffect(() => {
+		if (interactive) setRating(value);
+	}, [value]);
 
 	//Separate whole number part and decimal part
 	const rDown: number = Math.floor(rating);
 	const deci: number = rating-rDown;
 
 	const offset: number = deci * 100;
+
+	const handleHover = (rateValue: number): void => {
+		if (interactive && handleRate) handleRate(rateValue);
+	}
 	
 	const stars: JSX.Element[] = new Array(5).fill(1).map((item, index) => {
 		const add: string = (index < rDown) ?classes.full :"";
-		const half: string = (deci && index === rDown) ?classes.half :"";
-		return <Grid item key={index}>
-			<Star viewBox="0 0 28 26" className={[isView ?classes.bigStar :classes.star, add, half].join(' ')} />
+		const half: string = (deci && index === rDown) ?compute(offset, classes) :"";
+		return <Grid item key={index} className={isView ?classes.starContainer :""} onClick={handleOpen} onMouseOver={handleHover.bind(Rating, index+1)} >
+			<Star 
+				viewBox="0 0 28 26" 
+				className={[
+					isBig ?classes.bigStar :classes.star,  
+					interactive ?classes.interactive :"", 
+					add, 
+					half
+				].join(' ')} 
+			/>
 		</Grid>
 	});
 
 	return (
 		<Grid item className={classes.root}>
-			<svg height={0} width={0} viewBox="0 0 0 0">
-				<defs>
-				    <linearGradient id="halfStar" x1="0%" y1="100%" x2="0%" y2="0%">
-				      <stop offset="0%" style={{stopColor: "gold", "stopOpacity": 1}} />
-				      <stop offset={`${offset}%`} style={{stopColor: "gold", "stopOpacity": 1}} />
-				      <stop offset={`${offset}%`} style={{stopColor: "gray", "stopOpacity": 1}} />
-				    </linearGradient>
-				</defs>
-			</svg>
 			{stars}
+			{isView
+				?<Grid item>
+					<p className={[classes.rateNum, "rate-num"].join(' ')} onClick={handleOpen}> (115) </p>
+				</Grid>
+				:""
+			}
 		</Grid>
 	);
 }
