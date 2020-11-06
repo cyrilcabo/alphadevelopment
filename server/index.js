30b0d1dc9ad108f1593f01dd1bd88913bf7df744
Cyril Cabo <cyrilcabo@gmail.com>
2020-11-06 10:29:40 +0800

    split text-email to a different file

--- server/index.js
+++ server/index.js
@@ -17,6 +17,8 @@ const { MongoClient} = require('mongodb');
 const nodemailer = require('nodemailer');
 //HTML content
 const htmlEmail = require('./utils/mail_html.js');
+//Text content
+const textEmail = require('./utils/mail_text.js');
 
 //Import resolvers
 const { Query } = require('./Graphql/Resolvers/query');
@@ -61,24 +63,8 @@ const sendmail = async (contact) => {
     from: process.env.GMAIL_USER,
     to: contact,
     subject: "AlphaDevelopment Services",
-    text: `Thank you for choosing Alpha Development! 
-
-    	Let us further discuss your inquiries, by replying to this email. Currently, Alpha Development provides the following web development services: 
-    		* designing your web pages 
-    		* converting PSD Designs to HTML 
-    		* developing your frontend application 
-    		* designing your backend logic
-    		* connecting your application to a database
-    		* setting up your application on a server
-    		* building a fullstack application without you having to worry about anything else!
-
-    	We are looking forward to working with you! Cheers.
-
-
-    	What is Alpha Development?
-    	Alpha Development is a Tacloban-based web development company, and has started its journey since May 2020. Currently, it is a one-man company, but it has its eyes set on being the first established web development company in the region. Alpha Development's promise to its clients is to provide quality yet pocket-friendly products. Alpha Development products are fueled by customer's creative powers, as whatever you can imagine, Alpha Development will provide.
-	`,
-	html: htmlEmail,
+    text: textEmail,
+  	html: htmlEmail,
   });
 
   return info;
---
+++ server/utils/mail_text.js
@@ -0,0 +1,20 @@
+const textEmail =  `Thank you for choosing Alpha Development! 
+
+	Let us further discuss your inquiries, by replying to this email. Currently, Alpha Development provides the following web development services: 
+		* designing your web pages 
+		* converting PSD Designs to HTML 
+		* developing your frontend application 
+		* designing your backend logic
+		* connecting your application to a database
+		* setting up your application on a server
+		* building a fullstack application without you having to worry about anything else!
+
+	We are looking forward to working with you! Cheers.
+
+
+	What is Alpha Development?
+	Alpha Development is a Tacloban-based web development company, and has started its journey since May 2020. Currently, it is a one-man company, but it has its eyes set on being the first established web development company in the region. Alpha Development's promise to its clients is to provide quality yet pocket-friendly products. Alpha Development products are fueled by customer's creative powers, as whatever you can imagine, Alpha Development will provide.
+`;
+
+module.exports = textEmail;
+
