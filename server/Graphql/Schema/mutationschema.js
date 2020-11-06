716ef24f45e72f65ffd6105c627afe2289a2d1fa
Cyril Cabo <cyrilcabo@gmail.com>
2020-11-06 12:02:45 +0800

    added contact info mutation

--- server/Graphql/Schema/mutationschema.js
+++ server/Graphql/Schema/mutationschema.js
@@ -2,6 +2,7 @@ const mutationSchema = `
 	type Mutation {
 		subscribe(email: String!): StatusMessage!
 		like(productId: String!, name: String, msg: String, rating: Int!, prev: Int, id: String): Like!
+		contact(contact: String!, message: String!): StatusMessage!
 	}
 `;
 
