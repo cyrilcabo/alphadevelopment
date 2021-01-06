4094d17c7ebf7738e0e2e65d1e9c006e176939d2
Cyril Cabo <cyrilcabo@gmail.com>
2021-01-06 17:45:36 +0800

    added lazy loading

--- client/src/Components/Routes/routes.tsx
+++ client/src/Components/Routes/routes.tsx
@@ -2,40 +2,43 @@
 import {Switch, Route} from 'react-router-dom';
 
 //Utils
-import React from 'react';
+import React, {lazy, Suspense} from 'react';
+import LoadingScreen from '../LoadingScreen/loadingscreen';
 
 //Components
-import Index from '../Pages/index';
-import Products from '../Pages/products';
-import ViewProduct from '../Pages/viewproduct';
-import About from '../Pages/about';
-import Services from '../Pages/services';
+const Index = lazy(() => import('../Pages/index'));
+const Products = lazy(() => import('../Pages/products'));
+const ViewProduct = lazy(() => import('../Pages/viewproduct'));
+const About = lazy(() => import('../Pages/about'));
+const Services = lazy(() => import('../Pages/services'));
 
 const Routes = ():JSX.Element => {
 	return (
-		<Switch>
-			<Route path="/" exact>
-				<Index />
-			</Route>
-			<Route path="/products" exact>
-				<Products />
-			</Route>
-			<Route path="/products/view" exact>
-				<ViewProduct />
-			</Route>
-			<Route path="/about" exact>
-				<About />
-			</Route>
-			<Route path="/services" exact>
-				<Services />
-			</Route>
-			<Route path="">
-				<Index />
-			</Route>
-			<Route path="/*">
-				<Index />
-			</Route>
-		</Switch>
+		<Suspense fallback={<LoadingScreen />}>
+			<Switch>
+				<Route path="/" exact>
+					<Index />
+				</Route>
+				<Route path="/products" exact>
+					<Products />
+				</Route>
+				<Route path="/products/view" exact>
+					<ViewProduct />
+				</Route>
+				<Route path="/about" exact>
+					<About />
+				</Route>
+				<Route path="/services" exact>
+					<Services />
+				</Route>
+				<Route path="">
+					<Index />
+				</Route>
+				<Route path="/*">
+					<Index />
+				</Route>
+			</Switch>
+		</Suspense>
 	);
 }
 
