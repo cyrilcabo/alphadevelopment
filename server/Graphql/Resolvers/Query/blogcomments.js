a26da6fdf4127afcb4d7c17ee10bff127bb42bc6
Cyril Cabo <cyrilcabo@gmail.com>
2021-01-13 22:43:54 +0800

    added fetch-blog-comments resolver

---
+++ server/Graphql/Resolvers/Query/blogcomments.js
@@ -0,0 +1,15 @@
+const {ObjectId} = require('mongodb');
+
+const blogComments = (parent, args, context) => {
+	if (args._id) {
+		return context.db.then(db => {
+			return db.collection("blog_comments").find({blogId: ObjectId(args._id)}).skip(args.skip || 0).limit(10).toArray();
+		}).catch(err => {
+			throw new Error("Something went unexpected. Please try again.");
+		});
+	} else {
+		throw new Error ("Insufficient arguments!");
+	}
+}
+
+module.exports = blogComments;
