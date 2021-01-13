d64235d37a9403393782eaab074d50b5a6990d48
Cyril Cabo <cyrilcabo@gmail.com>
2021-01-13 22:44:39 +0800

    updated mutations, with blog-related components

--- server/Graphql/Resolvers/Mutation/index.js
+++ server/Graphql/Resolvers/Mutation/index.js
@@ -1,12 +1,16 @@
 const subscribe = require('./subscribe');
 const like = require('./like');
 const contact = require('./contact');
+const likeBlog = require('./likeblog');
+const addComment = require('./addcomment');
 
 
 const Mutation =  {
 	subscribe,
 	like,
 	contact,
+	likeBlog,
+	addComment
 }
 
 module.exports = {
