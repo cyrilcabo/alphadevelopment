1ecf81c015833cb7be662e151eedb6d376b5c8c7
Cyril Cabo <cyrilcabo@gmail.com>
2021-01-15 22:29:23 +0800

    updated commenting on blog

---
+++ client/src/Types/Blog/comment.tsx
@@ -0,0 +1,6 @@
+export default interface Comment {
+	_id: string,
+	name: string,
+	comment: string,
+	datePosted: Date,
+}
--- server/Graphql/Resolvers/Query/blogcomments.js
+++ server/Graphql/Resolvers/Query/blogcomments.js
@@ -3,7 +3,7 @@ const {ObjectId} = require('mongodb');
 const blogComments = (parent, args, context) => {
 	if (args._id) {
 		return context.db.then(db => {
-			return db.collection("blog_comments").find({blogId: ObjectId(args._id)}).skip(args.skip || 0).limit(10).toArray();
+			return db.collection("blog_comments").find({blogId: ObjectId(args._id)}).sort({datePosted: -1}).skip(args.skip || 0).limit(10).toArray();
 		}).catch(err => {
 			throw new Error("Something went unexpected. Please try again.");
 		});
