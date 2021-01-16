de01d5041b462f5b2612b5123184131766a2ebeb
Cyril Cabo <cyrilcabo@gmail.com>
2021-01-16 11:37:59 +0800

    fixed fetching of hot and featured blogs; added limit to results, as well

--- server/Graphql/Resolvers/Query/blogs.js
+++ server/Graphql/Resolvers/Query/blogs.js
@@ -1,6 +1,9 @@
 const {ObjectId} = require('mongodb');
 
 const blogs = (parent, args, context) => {
+	let sort = {};
+	if (args.featured) sort = {...sort, featured: -1};
+	if (args.hot) sort = {...sort, rating: -1};
 	return context.db.then(db => {
 		if (args.title || args.date || args.category) {
 			const {title, date, category} = args;
@@ -16,9 +19,9 @@ const blogs = (parent, args, context) => {
 					$all: category,
 				}
 			};
-			return db.collection("blogs_summary").find(query).skip(args.skip || 0).limit(10).toArray()
+			return db.collection("blogs_summary").find(query).sort({...sort, datePosted: -1}).skip(args.skip || 0).limit(args.limit || 10).toArray()
 		}
-		return db.collection("blogs_summary").find().skip(args.skip || 0).limit(10).toArray();
+		return db.collection("blogs_summary").find().sort({...sort, datePosted: -1}).skip(args.skip || 0).limit(args.limit || 10).toArray();
 	}).then(result => {
 		return result.map(item => {
 			return {
